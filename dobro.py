d54fa52dc4dd5614c9a32794a6bfa8d80d3420c6
evelynandradesena <0000112504441xsp@al.educacao.sp.gov.br>
2025-03-30 12:05:45 -0300

    Add files via upload

---
+++ dobro.py
@@ -0,0 +1,4 @@
+num1 = float(input("digite um numero: "))
+
+resultado = (num1)*2
+print("resultado final é: ", resultado)
---
+++ médiadenotas.py
@@ -0,0 +1,7 @@
+num1 = float(input("digite sua primeira nota: "))
+num2 = float(input("digite sua segunda nota: "))
+num3 = float(input("digite sua terceira nota: "))
+num4 = float(input("digite sua quarta nota: "))
+
+resultado = (num1 + num2 + num3 + num4)/4
+print("sua média final é: ", resultado)
